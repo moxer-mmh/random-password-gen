6125f1720d8a9b190bee7b67aebec507835693f2
moxer-mmh <moncefho10@gmail.com>
2024-10-04 17:00:54 +0100

    some small changes to the code and adding errors handling

--- rpg.py
+++ rpg.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def password_generator(length=8,uppercase=True,lowercase=True,numbers=False,special=False):
+def password_generator(length,uppercase,lowercase,numbers,special):
     characters = ""
     if uppercase:
         characters += string.ascii_uppercase
@@ -19,16 +19,52 @@ def password_generator(length=8,uppercase=True,lowercase=True,numbers=False,spec
 while True:
 
     print("--------------------------------------------------")
-    choix = input("Do you want to generate a password? (y/n): ")
+    try:
+        choix = input("Do you want to generate a password? (y/n): ")
+        assert choix == "y" or choix == "n"
+    except AssertionError:
+        print("Please enter 'y' or 'n'.")
+        continue
     print("--------------------------------------------------")
+
     if choix == "n":
         break
 
-    length = int(input("Enter the length of the password: "))
-    uppercase = input("Do you want uppercase letters in the password? (y/n): ")
-    lowercase = input("Do you want lowercase letters in the password? (y/n): ")
-    numbers = input("Do you want numbers in the password? (y/n): ")
-    special = input("Do you want special characters in the password? (y/n): ")
+    while True:
+        try:
+            length = int(input("Enter the length of the password: "))
+        except ValueError:
+            print("Please enter a number.")
+        else:
+            break
+
+    while True:
+        uppercase = input("Do you want uppercase letters in the password? (y/n): ")
+        if uppercase == "y" or uppercase == "n":
+            break
+        else:
+            print("Please enter 'y' or 'n'.")
+
+    while True:
+        lowercase = input("Do you want lowercase letters in the password? (y/n): ")
+        if lowercase == "y" or lowercase == "n":
+            break
+        else:
+            print("Please enter 'y' or 'n'.")
+
+    while True:
+        numbers = input("Do you want numbers in the password? (y/n): ")
+        if numbers == "y" or numbers == "n":
+            break
+        else:
+            print("Please enter 'y' or 'n'.")
+
+    while True:
+        special = input("Do you want special characters in the password? (y/n): ")
+        if special == "y" or special == "n":
+            break
+        else:
+            print("Please enter 'y' or 'n'.")
 
     uppercase = True if uppercase == "y" else False
     lowercase = True if lowercase == "y" else False
